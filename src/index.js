ddc0527b77351dce6e2cae6ec715fcfa1ec515cb
Moses Ogwo <mosesogwo@gmail.com>
2019-12-04 14:51:52 +0100

    add project.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './style.css';
 import todo from './todo';
+import project from './project';
+
+
 
---
+++ src/project.js
@@ -0,0 +1,6 @@
+const project = (name) => {
+  const todos = [];
+  return { name, todos };
+}
+
+export default project;
