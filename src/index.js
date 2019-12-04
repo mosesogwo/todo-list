9e92744dd6e036200990c88a4ffd142827c8e910
Moses Ogwo <mosesogwo@gmail.com>
2019-12-04 15:34:41 +0100

    implement new project feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import './style.css';
 import todo from './todo';
-import project from './project';
+import { project, projects, save, renderProjects } from './project';
 
+const pageLoad = () => {
+  const newProjectBtn = document.querySelector('.new-project');
+  const newProjectForm = document.querySelector('.new-project-form');
+  const newTodoBtn = document.querySelector('.new-todo');
+  const newTodoForm = document.querySelector('.new-todo-form');
 
+  newProjectBtn.addEventListener('click', () => {
+    newProjectForm.classList.toggle('hidden');
+  });
 
+  newProjectForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = newProjectForm.querySelector('#projectName').value;
+    if ( name !== "") {
+      const newProject = project(name);
+      newProject.addToProjects();
+      save();
+    } 
+  });
+
+  newTodoBtn.addEventListener('click', () => {
+    newTodoForm.classList.toggle('hidden');
+  });
+
+  newTodoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+  })
+}
+
+pageLoad();
--- src/project.js
+++ src/project.js
@@ -1,6 +1,27 @@
+const projects = JSON.parse(localStorage.getItem('projects_key')) || [];
+
 const project = (name) => {
   const todos = [];
-  return { name, todos };
+
+  const addToProjects = () => {
+    projects.push ({ name, todos });
+  }
+  return { name, todos, addToProjects };
 }
 
-export default project;
+const save = () => {
+  localStorage.setItem('projects_key', JSON.stringify(projects));
+  renderProjects();
+}
+
+const renderProjects = () => {
+  const projectsDiv = document.querySelector('.project');
+  projectsDiv.innerHTML = '';
+  projects.forEach((project) => {
+    let template = `
+    <a href="#" class="list-group-item list-group-item-action"> ${ project.name } <span class="badge badge-primary badge-pill">${ project.todos.length }</span></a>`;
+    projectsDiv.insertAdjacentHTML('beforeend', template);
+  });
+}
+
+export { project, projects, save, renderProjects };
