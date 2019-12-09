1872f7de03798bd2fedecaf0a5c9bcfd66ad2bdb
Moses Ogwo <mosesogwo@gmail.com>
2019-12-09 11:14:33 +0100

    add prepareForStorage, retrieveProjects method to project.js

--- src/project.js
+++ src/project.js
@@ -1,4 +1,32 @@
-const projects = [];
+import todo from './todo';
+
+const prepareForStorage = () => {
+  const projectsForStorage = JSON.parse(JSON.stringify(projects));
+  projects.forEach((p, pIdx) => {
+    p.todos.forEach((t,tIdx) => {
+      projectsForStorage[pIdx].todos[tIdx] = t.getTodoData();
+    })
+  })
+ return projectsForStorage
+};
+
+const retrieveProjects = () => {
+  const storedProjects = JSON.parse(localStorage.getItem('projects_key')) || [];
+  const newProjects = JSON.parse(JSON.stringify(storedProjects));
+  if (newProjects.length > 0){
+    newProjects.forEach((p) => {
+      p.todos.forEach((t, tIdx) => {
+        let newT = todo(t.title, t.description, t.dueDate, t.priority);
+        newT.completed = t.completed;
+        p.todos[tIdx] = newT;
+    });
+    });
+  }
+  return newProjects;
+}
+
+
+const projects = retrieveProjects();
 
 const project = (name) => {
   const todos = [];
@@ -9,4 +37,4 @@ const project = (name) => {
   return { id, name, todos, addToProjects };
 };
 
-export { project, projects };
+export { project, projects, prepareForStorage };
